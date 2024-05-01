12d281e7be57d2ee338985b5a9673829d66e0c18
Tommy Pham <TommyIsWerid@gmail.com>
2024-04-30 21:13:56 -0500

    Character Stats

---
+++ Assets/Scenes/Character.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public CharacterStat Strength;
+    public CharacterStat Agility;
+    public CharacterStat Intelligence;
+    public CharacterStat Vitality;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
